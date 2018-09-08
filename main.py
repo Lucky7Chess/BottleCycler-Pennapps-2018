feac6a8621285c970179f6e8ff33d0bbe132273e
maryberrie <changx964@gmail.com>
2018-09-08 10:49:11 -0400

    flask updates

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response, redirect
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("image.html")
+
+@app.route('/capture', methods = ["POST"])
+def capture():
+	if request.method == "POST":
+		img = request.get_json(force = True)
+
+		print(img)
+		return "jvhcxkvj"
 
 if __name__ == '__main__':
     app.run()
